1624e4ee610393e1b3200538202f1621ed5c9a63
Jayant <jayantm42@gmail.com>
2019-09-11 13:19:35 +0530

    Installing Firebase Auth, Login User and Register User + Rounded Corners UI

--- app/src/main/java/com/toon/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/toon/kotlinmessenger/LoginActivity.kt
@@ -1,27 +1,27 @@
 package com.toon.kotlinmessenger
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.toon.lbtaapplication.BaseActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity(), View.OnClickListener {
 
-    val TAG = "LoginActivity"
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        login_button_login.setOnClickListener {
+        login_button_login.setOnClickListener(this)
 
-            val email = email_editText_login.text.toString()
-            val password = password_editText_login.text.toString()
-
-            Log.d(TAG, "E-mail is: $email")
-            Log.d(TAG, "Password is: $password")
-
-        }
+        auth = FirebaseAuth.getInstance()
 
         create_account_textView_login.setOnClickListener {
 
@@ -32,4 +32,87 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun signIn(email: String, password: String) {
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        showProgressDialog()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                if (!task.isSuccessful) {
+                    Log.d(TAG, "Auth Failed")
+                }
+                hideProgressDialog()
+            }
+
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = email_editText_login.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            email_editText_login.error = "Required."
+            valid = false
+        } else {
+            email_editText_login.error = null
+        }
+
+        val password = password_editText_login.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            password_editText_login.error = "Required."
+            valid = false
+        } else {
+            password_editText_login.error = null
+        }
+
+        return valid
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        hideProgressDialog()
+        if (user != null) {
+
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+
+        } else {
+
+
+
+        }
+    }
+
+    override fun onClick(v: View) {
+        val i = v.id
+        when (i) {
+            R.id.login_button_login -> signIn(email_editText_login.text.toString(), password_editText_login.text.toString())
+        }
+    }
+
+    companion object {
+        private const val TAG = "LoginActivity"
+    }
+
 }
